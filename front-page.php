4fb4fe6dfe7152f7d86f573ed0a0de1990ff3edb
Ippei Otsuka <ippei20010526@gmail.com>
2024-04-08 10:45:29 +0900

    btnのコンポーネント追加

--- front-page.php
+++ front-page.php
@@ -1,14 +1,15 @@
 <?php get_header(); ?>
 <div class="l-wrapper">
-  <div class="p-mv">
-    <div class="p-mv__txt-box">
-      <h2 class="p-mv__head">MV見だし</h2>
-      <p class="p-mv__txt">
+  <div class="p-top-mv">
+    <div class="p-top-mv__txt-box">
+      <h2 class="p-top-mv__head">MV見だし</h2>
+      <p class="p-top-mv__txt">
         MVテキストMVテキストMVテキストMVテキスト
       </p>
     </div>
   </div>
   <main class="l-main">
+    <div class="btn-wrap"><a href="" class=" p-top-sec__btn c-btn">ボタン</a></div>
   </main>
 </div>
 <?php get_footer(); ?>
