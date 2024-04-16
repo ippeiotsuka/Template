bee3c0d26a829e6bd31d68209cac3957cdbe1959
Ippei Otsuka <ippei20010526@gmail.com>
2024-04-16 14:33:42 +0900

    jsに360px以下で等倍縮小させるコードを追加

--- assets/src/js/sub.js
+++ assets/src/js/sub.js
@@ -1,5 +1,23 @@
 export function functionName() {
   // ----------------------------------------------------------------------------------------------
+  // 360px以下は等倍縮小
+  // ----------------------------------------------------------------------------------------------
+  !(function () {
+    const viewport = document.querySelector('meta[name="viewport"]');
+    function switchViewport() {
+      const value =
+        window.outerWidth > 360
+          ? "width=device-width,initial-scale=1"
+          : "width=360";
+      if (viewport.getAttribute("content") !== value) {
+        viewport.setAttribute("content", value);
+      }
+    }
+    addEventListener("resize", switchViewport, false);
+    switchViewport();
+  })();
+
+  // ----------------------------------------------------------------------------------------------
   // ハンバーガーメニュー
   // ----------------------------------------------------------------------------------------------
   const header = document.querySelector(".l-header");
