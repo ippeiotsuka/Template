0f80e4ded83751618911d8039949b5f45e947cc7
Ippei Otsuka <ippei20010526@gmail.com>
2024-04-23 12:01:59 +0900

    header.php電話番号の自動リンク指定を修正

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description" content="ページの説明" /> <!-- 電話番号の自動リンク機能の指定 -->
+  <meta name="format-detection" content="telephone=no" /> <!-- 電話番号の自動リンク機能の指定 -->
   <link rel="icon" href="favicon.png" type="image/png" />
   <link rel="icon" href="favicon.svg" type="image/svg+xml" />
   <link rel="apple-touch-icon" href="webclip.png" /> <!-- Webページのショートカットを作成したとき、Webページをスマートフォンのホーム画面に追加したときに表示されるアイコン -->
